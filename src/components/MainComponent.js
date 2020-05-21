08e16cb71ae08774bf8ac5301da4213e9ebccd0f
Armando Reyes <risk.management.aseuam@gmail.com>
2020-05-21 00:52:45 -0500

    React Router -issues Menu OnClick

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,9 +1,11 @@
 import React, {Component} from 'react';
+import Home from './HomeComponent';
 import Menu from './MenuComponent';
 import DishDetail from './DishdetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import {DISHES} from '../shared/dishes';
+import { Switch, Route, Redirect} from 'react-router-dom';
 
 class Main extends Component {
 
@@ -12,7 +14,6 @@ class Main extends Component {
 
       this.state = {
         dishes: DISHES,
-        selectedDish: null
       };
     }
 
@@ -20,14 +21,22 @@ class Main extends Component {
         this.setState({selectedDish:dishId});
     }
 
-    render(){ return (
+    render(){ 
+      
+     const HomePage = () => {
+       return (
+         <Home />
+       );
+     } 
+    return (
     <div>
       <Header />
-      <Menu dishes = {this.state.dishes} 
-        onClick = {(dishId)=> this.onDishSelect(dishId)} />
-      <DishDetail 
-        dish ={this.state.dishes.filter((dish)=>dish.id === this.state.selectedDish )[0]} />
-        <Footer />
+      <Switch>
+        <Route path ="/home" component ={HomePage} />
+        <Route exact path ="/menu" component = {()=> <Menu dishes={this.state.dishes} />} />
+        <Redirect to ="/home" />
+      </Switch>
+      <Footer />
     </div>
     );
   }
