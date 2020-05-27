5ad4a4651a3314b7a8bf497e0dc5d9c35b193d6e
Armando Reyes <risk.management.aseuam@gmail.com>
2020-05-26 22:46:35 -0500

    React Redux Forms Revisited

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -180,35 +180,34 @@ import {Loading}  from './LoadingComponent';
             );
         }
         else if (props.dish != null)
-        
-        return (
-                <div className="Container">
-                     <div className='row'>
-                        <Breadcrumb>
-                            <BreadcrumbItem>
-                                <Link to='/menu'>Menu </Link>
-                            </BreadcrumbItem>
-                            <BreadcrumbItem active>
-                                {props.dish.name} 
-                            </BreadcrumbItem>
-                        </Breadcrumb>
-                        <div className = "col-12">
-                            <h3>{props.dish.name}</h3>
-                            <hr />
+            return (
+                    <div className="Container">
+                        <div className='row'>
+                            <Breadcrumb>
+                                <BreadcrumbItem>
+                                    <Link to='/menu'>Menu </Link>
+                                </BreadcrumbItem>
+                                <BreadcrumbItem active>
+                                    {props.dish.name} 
+                                </BreadcrumbItem>
+                            </Breadcrumb>
+                            <div className = "col-12">
+                                <h3>{props.dish.name}</h3>
+                                <hr />
+                            </div>
                         </div>
-                     </div>
-                    <div className ="row">
-                        <div className="col-12  col-md-5 m-1">
-                            <RenderDish dish={props.dish} />
-                        </div>
-                        <div className="col-12 col-md-5 ">
-                            <RenderComments comments = {props.dish?props.comments:null}
-                            addComment = {props.addComment}
-                            dishId={props.dish.id} />   
+                        <div className ="row">
+                            <div className="col-12  col-md-5 m-1">
+                                <RenderDish dish={props.dish} />
+                            </div>
+                            <div className="col-12 col-md-5 ">
+                                <RenderComments comments = {props.dish?props.comments:null}
+                                addComment = {props.addComment}
+                                dishId={props.dish.id} />   
+                            </div>
                         </div>
                     </div>
-                </div>
-        );
+            );
     }
 
 
